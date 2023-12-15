ff941e382f342287ff34cd25cdba8ae87d039c5b
datrelles <desarrollador2@massline.com.ec>
2023-12-15 12:41:24 -0500

    Bug Formule

--- src/components/Formule.js
+++ src/components/Formule.js
@@ -106,33 +106,6 @@ function Formules() {
     console.log(row)
   }
 
-  const handleDeleteRows = async (rowsDeleted) => {
-    const userResponse = window.confirm('¿Está seguro de eliminar la orden de compra?')
-    if (userResponse) {
-      await rowsDeleted.data.forEach((deletedRow) => {
-        const deletedRowIndex = deletedRow.dataIndex;
-        const deletedRowValue = purchaseOrders[deletedRowIndex];
-        fetch(`${API}/eliminar_orden_compra_total/${deletedRowValue.cod_po}/${enterpriseShineray}/PO`, {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + jwt
-          },
-        })
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Error en la llamada a la API');
-            }
-            enqueueSnackbar('¡Elementos eliminados exitosamente!', { variant: 'success' });
-          })
-          .catch(error => {
-            console.error(error);
-            enqueueSnackbar(error, { variant: 'error' });
-          });
-      });
-    }
-  };
-
   const handleChange2 = async (e) => {
     e.preventDefault();
     navigate('/newFormule');
@@ -191,7 +164,6 @@ function Formules() {
   const options = {
     responsive: 'standard',
     onRowClick: handleRowClick,
-    onRowsDelete: handleDeleteRows,
     textLabels: {
       body: {
         noMatch: "Lo siento, no se encontraron registros",
