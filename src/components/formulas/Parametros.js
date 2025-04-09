341ad5aa2d74ea3d75a57b033df8af69620ded08
raynerbeta <33467773+raynerbeta@users.noreply.github.com>
2025-04-09 12:57:32 -0500

    Agregar gestión de Parámetros

---
+++ src/components/formulas/Parametros.js
@@ -0,0 +1,485 @@
+import Navbar0 from "../Navbar0";
+import { toast } from 'react-toastify';
+import React, { useState, useEffect } from "react";
+import MUIDataTable from "mui-datatables";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
+import {FormControlLabel, Checkbox} from '@mui/material';
+import { useAuthContext } from "../../context/authContext";
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+
+
+const API = process.env.REACT_APP_API;
+
+function Parametros() {
+  const { jwt, userShineray, enterpriseShineray, systemShineray } = useAuthContext();
+  const [parametros, setParametros] = useState([])
+  const [menus, setMenus] = useState([])
+  const [openNew, setOpenNew] = useState(false);
+  const [openUpdate, setOpenUpdate] = useState(false);
+  const [codParametro, setCodParametro] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [descripcion, setDescripcion] = useState('');
+  const [estado, setEstado] = useState(true);
+
+  const navigate = useNavigate();
+
+  const getParametros = async () => {
+    try {
+      const res = await fetch(`${API}/modulo-formulas/empresas/${enterpriseShineray}/parametros`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + jwt
+          }
+        });
+
+      if (!res.ok) {
+        if (res.status === 401) {
+          toast.error('Sesión caducada.');
+        }
+      } else {
+        const data = await res.json();
+        setParametros(data)
+      }
+    } catch (error) {
+      toast.error('Sesión caducada. Por favor, inicia sesión nuevamente.');
+    }
+  }
+
+  const getMenus = async () => {
+    try {
+      const res = await fetch(`${API}/menus/${userShineray}/${enterpriseShineray}/${systemShineray}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + jwt
+          }
+        });
+
+      if (!res.ok) {
+        if (res.status === 401) {
+          toast.error('Sesión caducada.');
+        }
+      } else {
+        const data = await res.json();
+        setMenus(data)
+        console.log(data)
+      }
+    } catch (error) {
+    }
+  }
+
+  useEffect(() => {
+    document.title = 'Parámetros';
+    getParametros();
+    getMenus();
+  }, [openNew, openUpdate])
+
+  const handleRowClick = (rowData, rowMeta) => {
+    const row = parametros.filter(item => item.cod_formula === rowData[0])[0];
+    setCodParametro(row.cod_formula)
+    setNombre(row.nombre)
+    setDescripcion(row.observaciones ?? '')
+    setEstado(row.estado === 1)
+    handleClickOpenUpdate();
+  }
+
+  const handleDeleteRows = rowsDeleted => {
+    if (!window.confirm('¿Está seguro de eliminar el parámetro?')) {
+      return false;
+    }
+    const { data: deletedData } = rowsDeleted;
+    const deletedRowIndex = deletedData[0].index;
+    const deletedRowValue = parametros[deletedRowIndex];
+    const newParametros = parametros.filter((_, index) => index !== deletedRowIndex);
+    setParametros(newParametros);
+    fetch(`${API}/modulo-formulas/empresas/${enterpriseShineray}/formulas/${deletedRowValue.cod_parametro}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + jwt
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          getParametros();
+          return response.json();
+        }
+        toast.success('¡Elemento eliminado exitosamente!');
+      })
+      .then(data => {
+        if (data) {
+          toast.error(data.mensaje);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        toast.error('Ocurrió un error en la llamada a la API');
+      })
+    return true;
+  };
+
+  const renderText = (value) => {
+    const progress = parseInt(value);
+    const text = progress ? "Activo" : "Inactivo";
+    return (
+      <div>
+        <span>{text}</span>
+      </div>
+    );
+  };
+
+  const columns = [
+    {
+      name: "cod_parametro",
+      label: "Código"
+    },
+    {
+      name: "nombre",
+      label: "Nombre"
+    },
+    {
+      name: "descripcion",
+      label: "Descripción"
+    },
+    {
+      name: "estado",
+      label: "Estado",
+      options: {
+        customBodyRender: (value) => renderText(value),
+      },
+    },
+    {
+      name: "audit_fecha_ing",
+      label: "Fecha creación"
+    },
+  ]
+
+  const options = {
+    responsive: 'standard',
+    selectableRows: 'single',
+    onRowClick: handleRowClick,
+    onRowsDelete: handleDeleteRows,
+    textLabels: {
+      body: {
+        noMatch: "Lo siento, no se encontraron registros",
+        toolTip: "Ordenar",
+        columnHeaderTooltip: column => `Ordenar por ${column.label}`
+      },
+      pagination: {
+        next: "Siguiente",
+        previous: "Anterior",
+        rowsPerPage: "Filas por página:",
+        displayRows: "de"
+      },
+      toolbar: {
+        search: "Buscar",
+        downloadCsv: "Descargar CSV",
+        print: "Imprimir",
+        viewColumns: "Ver columnas",
+        filterTable: "Filtrar tabla"
+      },
+      filter: {
+        all: "Todos",
+        title: "FILTROS",
+        reset: "REINICIAR"
+      },
+      viewColumns: {
+        title: "Mostrar columnas",
+        titleAria: "Mostrar/Ocultar columnas de tabla"
+      },
+      selectedRows: {
+        text: "fila(s) seleccionada(s)",
+        delete: "Borrar",
+        deleteAria: "Borrar fila seleccionada"
+      }
+    }
+
+  }
+
+  const getMuiTheme = () =>
+    createTheme({
+      components: {
+        MuiTableCell: {
+          styleOverrides: {
+            root: {
+              paddingLeft: '3px', // Relleno a la izquierda
+              paddingRight: '3px',
+              paddingTop: '0px', // Ajusta el valor en el encabezado si es necesario
+              paddingBottom: '0px',
+              backgroundColor: '#00000',
+              whiteSpace: 'nowrap',
+              flex: 1,
+              borderBottom: '1px solid #ddd',
+              borderRight: '1px solid #ddd',
+              fontSize: '14px'
+            },
+            head: {
+              backgroundColor: 'firebrick', // Color de fondo para las celdas de encabezado
+              color: '#ffffff', // Color de texto para las celdas de encabezado
+              fontWeight: 'bold', // Añadimos negrita para resaltar el encabezado
+              paddingLeft: '0px',
+              paddingRight: '0px',
+              fontSize: '12px'
+            },
+          }
+        },
+        MuiTable: {
+          styleOverrides: {
+            root: {
+              borderCollapse: 'collapse', // Fusionamos los bordes de las celdas
+            },
+          },
+        },
+        MuiTableHead: {
+          styleOverrides: {
+            root: {
+              borderBottom: '5px solid #ddd', // Línea inferior más gruesa para el encabezado
+            },
+          },
+        },
+        MuiToolbar: {
+          styleOverrides: {
+            regular: {
+              minHeight: '10px',
+            }
+          }
+        }
+      }
+    });
+
+  const handleClickOpenNew = () => {
+    setOpenNew(true);
+    setCodParametro('')
+    setNombre('')
+    setDescripcion('')
+    setEstado(true)
+  };
+
+  const handleClickCloseNew = () => {
+    setOpenNew(false);
+  };
+
+  const handleClickOpenUpdate = () => {
+    setOpenUpdate(true);
+  };
+
+  const handleClickCloseUpdate = () => {
+    setOpenUpdate(false);
+  };
+
+  const handleCreate = async (e) => {
+    e.preventDefault();
+    const res = await fetch(`${API}/modulo-formulas/empresas/${enterpriseShineray}/formulas`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + jwt
+      },
+      body: JSON.stringify({
+        empresa: enterpriseShineray,
+        cod_parametro: codParametro,
+        nombre,
+        descripcion,
+      })
+    });
+    const { mensaje } = await res.json();
+    if(res.ok){
+      toast.success(mensaje)
+      setOpenNew(false)
+      setCodParametro('')
+      setNombre('')
+      setDescripcion('')
+      setEstado(true)
+    }else{
+      toast.error(mensaje)
+    }
+  }
+
+  const handleUpdate = async (e)=>{
+    e.preventDefault();
+    const res = await fetch(`${API}/modulo-formulas/empresas/${enterpriseShineray}/parametros/${codParametro}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + jwt
+      },
+      body: JSON.stringify({
+        nombre,
+        descripcion,
+        estado,
+      })
+    })
+    if (res.ok) {
+      toast.success('Actualización exitosa')
+      setOpenUpdate(false)
+      setCodParametro('')
+      setNombre('')
+      setDescripcion('')
+      setEstado(true)
+    } else {
+      const { mensaje } = await res.json();
+      toast.error(mensaje)
+    }
+  }
+
+  return (
+    <div style={{ marginTop: '150px', top: 0, left: 0, width: "100%", zIndex: 1000 }}>
+      <Navbar0 menus={menus} />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'right',
+          '& > *': {
+            m: 1,
+          },
+        }}
+      >
+        <ButtonGroup variant="text" aria-label="text button group" >
+          <Button onClick={() => { navigate('/dashboard') }}>Módulos</Button>
+        </ButtonGroup>
+      </Box>
+      <div style={{ display: 'flex', alignItems: 'right', justifyContent: 'space-between' }}>
+        <button
+          className="btn btn-primary btn-block"
+          type="button"
+          style={{ marginBottom: '10px', marginTop: '10px', backgroundColor: 'firebrick', borderRadius: '5px' }}
+          onClick={handleClickOpenNew}>
+          <AddIcon /> Nuevo
+        </button>
+      </div>
+      <ThemeProvider theme={getMuiTheme()}>
+        <MUIDataTable
+          title={"Fórmulas"}
+          data={parametros}
+          columns={columns}
+          options={options}
+        />
+      </ThemeProvider>
+      <Dialog open={openNew} onClose={handleClickCloseNew}>
+        <DialogTitle>Registrar Parámetro</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                margin="dense"
+                id="cod_parametro"
+                label="Código"
+                type="text"
+                placeholder="PARAM###"
+                fullWidth
+                value={codParametro}
+                onChange={(e => setCodParametro(e.target.value))}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                margin="dense"
+                id="nombre"
+                label="Nombre"
+                type="text"
+                fullWidth
+                value={nombre}
+                onChange={(e => setNombre(e.target.value))}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                margin="dense"
+                id="descripcion"
+                label="Descripción"
+                type="text"
+                fullWidth
+                value={descripcion}
+                onChange={(e => setDescripcion(e.target.value))}
+              />
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClickCloseNew} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleCreate} style={{ marginBottom: '10px', marginTop: '10px', backgroundColor: 'firebrick', color: 'white', height: '30px', width: '100px', borderRadius: '5px', marginRight: '15px' }}>
+            Crear
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={openUpdate} onClose={handleClickCloseUpdate}>
+        <DialogTitle>Actualizar Parámetro</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                disabled
+                margin="dense"
+                id="cod_parametro"
+                label="Código"
+                type="text"
+                fullWidth
+                value={codParametro}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                margin="dense"
+                id="nombre"
+                label="Nombre"
+                type="text"
+                fullWidth
+                value={nombre}
+                onChange={(e => setNombre(e.target.value))}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                margin="dense"
+                id="descripcion"
+                label="Descripción"
+                type="text"
+                fullWidth
+                value={descripcion}
+                onChange={(e => setDescripcion(e.target.value))}
+              />
+            </Grid>
+          </Grid>
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} >
+          <FormControlLabel control={
+            <Checkbox 
+            label="Estado"
+            checked={estado}
+            onChange={(e) => {
+              setEstado(e.target.checked)
+            }}
+            />
+          }
+          label="Activo"
+          />
+            
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClickCloseUpdate} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleUpdate} style={{ marginBottom: '10px', marginTop: '10px', backgroundColor: 'firebrick', color: 'white', height: '30px', width: '100px', borderRadius: '5px', marginRight: '15px' }}>
+            Actualizar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
+
+export default Parametros
