830550db8dd09bb6248edfe53a79d6196a31df0b
raynerbeta <33467773+raynerbeta@users.noreply.github.com>
2025-06-01 17:33:01 -0500

    Crear componentes de presupuesto y cobertura

---
+++ src/components/importaciones/presupuesto-cantidades.js
@@ -0,0 +1,33 @@
+import { toast } from "react-toastify";
+import { useState, useEffect, useMemo } from "react";
+import { useAuthContext } from "../../context/authContext";
+import API from "../../services/modulo-formulas";
+import Header from "../formulas/common/header";
+import MainComponent from "../formulas/common/main-component";
+
+export default function PresupuestoCantidades() {
+  const { jwt, userShineray, enterpriseShineray, systemShineray } =
+    useAuthContext();
+  const APIService = useMemo(
+    () => new API(jwt, userShineray, enterpriseShineray, systemShineray),
+    [jwt]
+  );
+  const [menus, setMenus] = useState([]);
+
+  const getMenus = async () => {
+    try {
+      setMenus(await APIService.getMenus());
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
+  const header = <Header menus={menus} />;
+
+  useEffect(() => {
+    document.title = "Presupuesto de Cantidades";
+    getMenus();
+  }, []);
+
+  return <MainComponent components={[header]} />;
+}
