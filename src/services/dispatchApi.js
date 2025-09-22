f0d90e4bd85092fd2346c282709f9a203f54579f
JHOFER1 <fernando.palaguachij@gmail.com>
2025-09-22 16:13:21 -0500

    gato

--- src/services/dispatchApi.js
+++ src/services/dispatchApi.js
@@ -5,7 +5,7 @@ import axios from "axios";
 // =====================
 const api = axios.create({
   baseURL: process.env.REACT_APP_API,
-  timeout: 15000,
+  timeout: 150000,
 });
 
 // Setter del token
